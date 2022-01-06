3852c024e7e7ccb644fa48fdadfb9dcc15d87b7b
Melanie Wong <82552071+melwong-08@users.noreply.github.com>
2022-01-06 15:48:13 -0800

    Update theaters.service.js

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,6 +1,17 @@
 const knex = require("../db/connection");
 
 
-module.exports={
-    
+function list() {
+    return knex("theaters").select("*");
+}
+
+function addMovies(theaterId) {
+    return knex("movies_theaters as mt")
+        .join("movies as m", "mt.movie_id", "m.movie_id")
+        .where("theater_id", theaterId)
+        .select("m.*", "mt.created_at", "mt.updated_at", "mt.is_showing", "mt.theater_id" )
+}
+
+module.exports = {
+    list, addMovies,
 }
