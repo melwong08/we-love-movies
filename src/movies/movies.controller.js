8e8536e9d5941c5c3674707e43b0a87c2619dc07
Melanie Wong <82552071+melwong-08@users.noreply.github.com>
2022-01-05 17:24:48 -0800

    Update movies.controller.js

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,13 +1,29 @@
 const moviesService = require("./movies.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const { reset } = require("nodemon");
 
-function list(req, res, next) {
-  moviesService
-    .list()
-    .then((data) => res.json({ data }))
-    .catch(next);
+async function list(req, res, next) {
+    const data = await moviesService.list(req.query.is_showing);
+    res.json({ data })
+}
+
+async function movieExists(req, res, next){
+    const {movieId} = req.params;
+    const movie = await moviesService.read(movieId);
+    if(movie) {
+        res.locals.movieId = movieId;
+        res.locals.movie = movie;
+        return next();
+    }
+    next({status: 404, message: `Movie cannot be found.`})
+}
+
+function read(req, res){
+    const {movie} = res.locals;
+    res.json({data:movie})
 }
 
 module.exports = {
-  list,
+  read: [asyncErrorBoundary(movieExists), read],
+  list: asyncErrorBoundary(list),
 };
