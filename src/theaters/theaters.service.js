c63ea727b3997551a3a3807f96ce32d510dceb96
Melanie Wong <82552071+melwong-08@users.noreply.github.com>
2022-01-06 21:12:21 -0800

    fixed errors

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -9,7 +9,7 @@ function addMovies(theaterId) {
     return knex("movies_theaters as mt")
         .join("movies as m", "mt.movie_id", "m.movie_id")
         .where("theater_id", theaterId)
-        .select("m.*", "mt.created_at", "mt.updated_at", "mt.is_showing", "mt.theater_id" )
+        .select("m.*", "mt.is_showing", "mt.theater_id" )
 }
 
 module.exports = {
