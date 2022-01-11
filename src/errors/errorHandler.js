e6a5870a3902e6bd97da5c95ecbc1d76d6a85a9b
Melanie Wong <82552071+melwong-08@users.noreply.github.com>
2022-01-11 11:26:42 -0800

    update error files

--- src/errors/errorHandler.js
+++ src/errors/errorHandler.js
@@ -1,7 +1,6 @@
 function errorHandler(error, request, response, next) {
-  console.log("errorHandler", request.body)
-    const { status = 500, message = "Something went wrong!" } = error;
-    response.status(status).json({ error: message });
-  }
-  
-  module.exports = errorHandler;
+  const { status = 500, message = "Something went wrong!" } = error;
+  response.status(status).json({ error: message });
+}
+
+module.exports = errorHandler;
--- src/errors/methodNotAllowed.js
+++ src/errors/methodNotAllowed.js
@@ -1,9 +1,8 @@
 function methodNotAllowed(req, res, next) {
-  console.log("methodnotallowed", req.body)  
   next({
-      status: 405,
-      message: `${req.method} not allowed for ${req.originalUrl}`,
-    });
-  }
-  
-  module.exports = methodNotAllowed;
+    status: 405,
+    message: `${req.method} not allowed for ${req.originalUrl}`,
+  });
+}
+
+module.exports = methodNotAllowed;
